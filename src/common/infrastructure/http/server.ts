45609aa7895b702099fe7abfeeb3313585563afc
Otavin96 <167653370+Otavin96@users.noreply.github.com>
2024-11-10 21:00:06 -0300

    Configurando a conexão do TypeORM com o servidor Postgres

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,7 +1,15 @@
 import { env } from '../env'
+import { dataSource } from '../typeorm'
 import { app } from './app'
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on ${env.PORT}`)
-  console.log('API docs available at GET /docs')
-})
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(env.PORT, () => {
+      console.log(`Server is running on ${env.PORT}`)
+      console.log('API docs available at GET /docs')
+    })
+  })
+  .catch(error => {
+    console.error('Error initializinf data source: ', error)
+  })
