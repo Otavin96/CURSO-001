f76f5b95514217b7b1aeab82407f0b7091672e68
Otavin96 <167653370+Otavin96@users.noreply.github.com>
2024-11-18 21:04:05 -0300

    Trabalhando com a parte de testes unitarios

--- src/common/domain/repositories/in-memory.repository.spec.ts
+++ src/common/domain/repositories/in-memory.repository.spec.ts
@@ -50,19 +50,105 @@ describe('InMemoryRepository unit test', () => {
     }
   })
 
-  it('should create a new model', () => {
-    const result = sut.create(props)
-    expect(result.name).toStrictEqual('test name')
+  describe('create', () => {
+    it('should create a new model', () => {
+      const result = sut.create(props)
+      expect(result.name).toStrictEqual('test name')
+    })
   })
 
-  it('should inserts a new model', async () => {
-    const result = await sut.insert(model)
-    expect(result).toStrictEqual(sut.items[0])
+  describe('insert', () => {
+    it('should inserts a new model', async () => {
+      const result = await sut.insert(model)
+      expect(result).toStrictEqual(sut.items[0])
+    })
+  })
+  describe('findById', () => {
+    it('Should throw error when id not found', async () => {
+      await expect(sut.findById('fake_id')).rejects.toThrow(
+        new NotFoundError('Model not found using ID fake_id'),
+      )
+
+      const id = randomUUID()
+
+      await expect(sut.findById(id)).rejects.toThrow(
+        new NotFoundError(`Model not found using ID ${id}`),
+      )
+    })
+
+    it('should find a model by id', async () => {
+      const data = await sut.insert(model)
+      const result = await sut.findById(data.id)
+      expect(result).toStrictEqual(data)
+    })
   })
 
-  it('Should throw error when id not found', async () => {
-    await expect(sut.findById('fake_id')).rejects.toThrow(
-      new NotFoundError('Model not found using ID fake_id'),
-    )
+  describe('update', () => {
+    it('should update an model', async () => {
+      const data = await sut.insert(model)
+
+      const modelUpdate = {
+        id: data.id,
+        name: 'updated name',
+        price: 2000,
+        created_at,
+        updated_at,
+      }
+
+      const result = await sut.update(modelUpdate)
+      expect(result).toStrictEqual(sut.items[0])
+    })
+  })
+
+  describe('delete', () => {
+    it('Should throw error when id not found', async () => {
+      await expect(sut.delete('fake_id')).rejects.toThrow(
+        new NotFoundError('Model not found using ID fake_id'),
+      )
+
+      const id = randomUUID()
+
+      await expect(sut.delete(id)).rejects.toThrow(
+        new NotFoundError(`Model not found using ID ${id}`),
+      )
+    })
+
+    it('should delete an model', async () => {
+      const data = await sut.insert(model)
+      expect(sut.items.length).toBe(1)
+      await sut.delete(data.id)
+      expect(sut.items.length).toBe(0)
+    })
+  })
+
+  describe('applyFilter', () => {
+    it('should no filter items when filter param is null', async () => {
+      const items = [model]
+      const spyFilterMethod = jest.spyOn(items, 'filter' as any)
+      const result = await sut['applyFilter'](items, null)
+      expect(spyFilterMethod).not.toHaveBeenCalled()
+      expect(result).toStrictEqual(items)
+    })
+
+    it('should filter the date using filter name', async () => {
+      const items = [
+        { id: randomUUID(), name: 'test', price: 10, created_at, updated_at },
+        { id: randomUUID(), name: 'TEST', price: 20, created_at, updated_at },
+        { id: randomUUID(), name: 'fake', price: 30, created_at, updated_at },
+      ]
+
+      const spyFilterMethod = jest.spyOn(items, 'filter' as any)
+      let result = await sut['applyFilter'](items, 'TEST')
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1)
+      expect(result).toStrictEqual([items[0], items[1]])
+
+      result = await sut['applyFilter'](items, 'test')
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2)
+      expect(result).toStrictEqual([items[0], items[1]])
+
+      result = await sut['applyFilter'](items, 'no-filter')
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3)
+      expect(result).toHaveLength(0)
+    })
   })
 })
