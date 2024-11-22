f228f4670fea88123756492167caea46d4550ea7
Otavin96 <otaviolazzarotto@icloud.com>
2024-11-22 12:57:52 -0300

    Implementando classe de teste

--- src/products/infrastruture/typeorm/repositories/products-typeorm.repository.int-spec.ts
+++ src/products/infrastruture/typeorm/repositories/products-typeorm.repository.int-spec.ts
@@ -1,7 +1,23 @@
-describe('ProductsTypeormRepository integration tests', () => {
-  let productsRepository: ProductsTypeormRepository
+import { Product } from "../entities/products.entity"
+import { testDataSource } from "../testing/data-source"
+import { ProductTypeormRepository } from "./products-typeorm.repository"
 
-  beforeEach(() => {})
+describe('ProductsTypeormRepository integration tests', () => {
+  let ormRepository: ProductTypeormRepository
+
+  beforeAll(async () => {
+    await testDataSource.initialize()
+  })
+
+  afterAll( async () => {
+    await testDataSource.destroy()
+  })
+
+  beforeEach( async () => {
+    await testDataSource.manager.query('DELETE FROM products')
+    ormRepository = new ProductTypeormRepository()
+    ormRepository.productsRepository = testDataSource.getRepository(Product) 
+  })
 
   describe('method', () => {
     it('testing', () => {})
