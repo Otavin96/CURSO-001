dd3c545cda251cc4cd3c70e8f4e162e638f1f2ae
Otavin96 <otaviolazzarotto@icloud.com>
2024-11-22 19:45:56 -0300

    Finalizando criação de teste de integration

--- src/products/infrastruture/typeorm/repositories/products-typeorm.repository.int-spec.ts
+++ src/products/infrastruture/typeorm/repositories/products-typeorm.repository.int-spec.ts
@@ -1,6 +1,9 @@
+import { randomUUID } from "node:crypto"
 import { Product } from "../entities/products.entity"
 import { testDataSource } from "../testing/data-source"
 import { ProductTypeormRepository } from "./products-typeorm.repository"
+import { NotFoundError } from "@/common/domain/erros/not-found-error"
+import { ProductsDataBuilder } from "../../testing/helpers/products-data-builder"
 
 describe('ProductsTypeormRepository integration tests', () => {
   let ormRepository: ProductTypeormRepository
@@ -19,7 +22,23 @@ describe('ProductsTypeormRepository integration tests', () => {
     ormRepository.productsRepository = testDataSource.getRepository(Product) 
   })
 
-  describe('method', () => {
-    it('testing', () => {})
+  describe('findById', () => {
+    it('should generate an error when  the product is not found', async () => {
+      const id = randomUUID()
+
+      await expect(ormRepository.findById(id)).rejects.toThrow(
+        new NotFoundError(`Product not found using ID ${id}`)
+      )
+    })
+
+    it('should finds a product by id', async () => {
+      const data = ProductsDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+      
+      const result = await ormRepository.findById(product.id)
+      expect(result.id).toEqual(product.id)
+      expect(result.name).toEqual(product.name)
+    })
   })
 })
