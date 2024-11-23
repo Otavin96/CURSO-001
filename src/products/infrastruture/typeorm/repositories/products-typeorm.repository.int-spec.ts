b437eb70145bbe9938ac22f2e4ffe47a0049257a
Otavin96 <167653370+Otavin96@users.noreply.github.com>
2024-11-22 22:26:53 -0300

    Criando outros metodos e testes

--- src/products/infrastruture/typeorm/repositories/products-typeorm.repository.int-spec.ts
+++ src/products/infrastruture/typeorm/repositories/products-typeorm.repository.int-spec.ts
@@ -1,9 +1,9 @@
-import { randomUUID } from "node:crypto"
-import { Product } from "../entities/products.entity"
-import { testDataSource } from "../testing/data-source"
-import { ProductTypeormRepository } from "./products-typeorm.repository"
-import { NotFoundError } from "@/common/domain/erros/not-found-error"
-import { ProductsDataBuilder } from "../../testing/helpers/products-data-builder"
+import { randomUUID } from 'node:crypto'
+import { Product } from '../entities/products.entity'
+import { testDataSource } from '../testing/data-source'
+import { ProductTypeormRepository } from './products-typeorm.repository'
+import { NotFoundError } from '@/common/domain/erros/not-found-error'
+import { ProductsDataBuilder } from '../../testing/helpers/products-data-builder'
 
 describe('ProductsTypeormRepository integration tests', () => {
   let ormRepository: ProductTypeormRepository
@@ -12,14 +12,14 @@ describe('ProductsTypeormRepository integration tests', () => {
     await testDataSource.initialize()
   })
 
-  afterAll( async () => {
+  afterAll(async () => {
     await testDataSource.destroy()
   })
 
-  beforeEach( async () => {
+  beforeEach(async () => {
     await testDataSource.manager.query('DELETE FROM products')
     ormRepository = new ProductTypeormRepository()
-    ormRepository.productsRepository = testDataSource.getRepository(Product) 
+    ormRepository.productsRepository = testDataSource.getRepository(Product)
   })
 
   describe('findById', () => {
@@ -27,7 +27,7 @@ describe('ProductsTypeormRepository integration tests', () => {
       const id = randomUUID()
 
       await expect(ormRepository.findById(id)).rejects.toThrow(
-        new NotFoundError(`Product not found using ID ${id}`)
+        new NotFoundError(`Product not found using ID ${id}`),
       )
     })
 
@@ -35,7 +35,7 @@ describe('ProductsTypeormRepository integration tests', () => {
       const data = ProductsDataBuilder({})
       const product = testDataSource.manager.create(Product, data)
       await testDataSource.manager.save(product)
-      
+
       const result = await ormRepository.findById(product.id)
       expect(result.id).toEqual(product.id)
       expect(result.name).toEqual(product.name)
@@ -44,21 +44,78 @@ describe('ProductsTypeormRepository integration tests', () => {
 
   describe('create', () => {
     it('should create a new product object', () => {
-       const data = ProductsDataBuilder({ name: 'Product 1' })
-       const result = ormRepository.create(data)
-       expect(result.name).toEqual(data.name)
+      const data = ProductsDataBuilder({ name: 'Product 1' })
+      const result = ormRepository.create(data)
+      expect(result.name).toEqual(data.name)
     })
-    
   })
 
   describe('insert', () => {
     it('should insert a new product', async () => {
-       const data = ProductsDataBuilder({ name: 'Product 1' })
-       const result = await ormRepository.insert(data)
-       expect(result.name).toEqual(data.name)
+      const data = ProductsDataBuilder({ name: 'Product 1' })
+      const result = await ormRepository.insert(data)
+      expect(result.name).toEqual(data.name)
     })
-    
   })
 
-  
+  describe('update', () => {
+    it('should generate an error when  the product is not found', async () => {
+      const data = ProductsDataBuilder({})
+
+      await expect(ormRepository.update(data)).rejects.toThrow(
+        new NotFoundError(`Product not found using ID ${data.id}`),
+      )
+    })
+
+    it('should update a product', async () => {
+      const data = ProductsDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+      product.name = 'nome atualizado'
+
+      const result = await ormRepository.update(product)
+      expect(result.name).toEqual('nome atualizado')
+    })
+  })
+
+  describe('delete', () => {
+    it('should generate an error when  the product is not found', async () => {
+      const id = randomUUID()
+      await expect(ormRepository.delete(id)).rejects.toThrow(
+        new NotFoundError(`Product not found using ID ${id}`),
+      )
+    })
+
+    it('should delete a product', async () => {
+      const data = ProductsDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      await ormRepository.delete(data.id)
+
+      const result = await testDataSource.manager.findOneBy(Product, {
+        id: data.id,
+      })
+      expect(result).toBeNull()
+    })
+  })
+
+  describe('findByName', () => {
+    it('should generate an error when  the product is not found', async () => {
+      const name = 'Product 1'
+
+      await expect(ormRepository.findByName(name)).rejects.toThrow(
+        new NotFoundError(`Product not found using name ${name}`),
+      )
+    })
+
+    it('should finds a product by name', async () => {
+      const data = ProductsDataBuilder({ name: 'Product 1' })
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      const result = await ormRepository.findByName(data.name)
+      expect(result.name).toEqual('Product 1')
+    })
+  })
 })
--- src/products/infrastruture/typeorm/repositories/products-typeorm.repository.ts
+++ src/products/infrastruture/typeorm/repositories/products-typeorm.repository.ts
@@ -12,6 +12,7 @@ import { Repository } from 'typeorm'
 import { Product } from '../entities/products.entity'
 import { dataSource } from '@/common/infrastructure/typeorm'
 import { NotFoundError } from '@/common/domain/erros/not-found-error'
+import { ConflictError } from '@/common/domain/erros/conflict-error'
 
 export class ProductTypeormRepository implements ProductsRepository {
   sortableFields: string[] = ['name', 'created_at']
@@ -21,16 +22,28 @@ export class ProductTypeormRepository implements ProductsRepository {
     this.productsRepository = dataSource.getRepository(Product)
   }
 
-  findByName(name: string): Promise<ProductModel> {
-    throw new Error('Method not implemented.')
+  async findByName(name: string): Promise<ProductModel> {
+    const product = await this.productsRepository.findOneBy({ name })
+
+    if (!product) {
+      throw new NotFoundError(`Product not found using name ${name}`)
+    }
+
+    return product
   }
 
   findAllByIds(productIds: ProductId[]): Promise<ProductModel[]> {
     throw new Error('Method not implemented.')
   }
 
-  confictingName(name: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async confictingName(name: string): Promise<void> {
+    const product = await this.productsRepository.findOneBy({ name })
+
+    if (product) {
+      throw new ConflictError('Name already used by another product')
+    }
+
+    return product
   }
 
   create(props: CreateProductProps): ProductModel {
@@ -45,12 +58,15 @@ export class ProductTypeormRepository implements ProductsRepository {
     return this._get(id)
   }
 
-  update(model: ProductModel): Promise<ProductModel> {
-    throw new Error('Method not implemented.')
+  async update(model: ProductModel): Promise<ProductModel> {
+    await this._get(model.id)
+    await this.productsRepository.update({ id: model.id }, model)
+    return model
   }
 
-  delete(id: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete(id: string): Promise<void> {
+    await this._get(id)
+    await this.productsRepository.delete({ id })
   }
 
   search(props: SearchInput): Promise<SearchOutput<ProductModel>> {
@@ -58,9 +74,9 @@ export class ProductTypeormRepository implements ProductsRepository {
   }
 
   protected async _get(id: string): Promise<ProductModel> {
-    const product = await this.productsRepository.findOneBy({id})
+    const product = await this.productsRepository.findOneBy({ id })
 
-    if(!product) {
+    if (!product) {
       throw new NotFoundError(`Product not found using ID ${id}`)
     }
 
